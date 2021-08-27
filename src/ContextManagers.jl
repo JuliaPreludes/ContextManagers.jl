9d95f3f327581dd85eac466cf720ef8c7a3dee2f
Takafumi Arakaki <aka.tkf@gmail.com>
2021-08-27 17:05:03 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/ContextManagers.jl
@@ -0,0 +1,11 @@
+baremodule ContextManagers
+
+module Internal
+
+using ..ContextManagers: ContextManagers
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule ContextManagers
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ContextManagers
+using Test
+
+@testset "ContextManagers.jl" begin
+    # Write your tests here.
+end
