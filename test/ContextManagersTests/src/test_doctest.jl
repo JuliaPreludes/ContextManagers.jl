4d0be129ad6b9f98384e47382e6f9b33f48c07f5
Takafumi Arakaki <aka.tkf@gmail.com>
2021-08-29 02:05:17 -0400

    Ignore doctest in old Julia

--- test/ContextManagersTests/src/test_doctest.jl
+++ test/ContextManagersTests/src/test_doctest.jl
@@ -5,6 +5,10 @@ using Test
 using ContextManagers
 
 function test()
+    if VERSION < v"1.5"
+        @test_broken false
+        return
+    end
     doctest(ContextManagers; manual = true)
 end
 
